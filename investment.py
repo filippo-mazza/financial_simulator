85fb48b21e677f67ec08380296ecf735ec3e86bd
Filippo Mazza <mazza.networking@gmail.com>
2025-01-19 22:21:24 +0100

    Add config to investment - need yaml still

--- investment.py
+++ investment.py
@@ -2,22 +2,19 @@ import random
 import matplotlib.pyplot as plt
 
 class Investment:
-    # Define typology details with mean and standard deviation for annual gains
-    TOPOLOGY_DETAILS = {
-        'stock': {'mean': 8.0, 'std_dev': 20.0},         # Stock: mean of 8% annual gain, std deviation of 20%
-        'real_estate': {'mean': 4.0, 'std_dev': 8.0},   # Real Estate: mean of 4% annual gain, std deviation of 8%
-        'bonds': {'mean': 2.5, 'std_dev': 5.0},         # Bonds: mean of 2.5% annual gain, std deviation of 5%
-        'safe': {'mean': 2, 'std_dev': 1.0}
-        # ... add other typologies as needed
-    }
-    DIVIDEND = 0.0237 # typical, pct
 
-    def __init__(self, name: str, typology: str, initial_cost: float, monthly_contribution: float, current_value: float = None):
-        if typology not in self.TOPOLOGY_DETAILS:
-            raise ValueError(f"Invalid typology. Must be one of {list(self.TOPOLOGY_DETAILS.keys())}")
+
+    def __init__(self, config,
+                 name: str, typology: str, initial_cost: float, monthly_contribution: float, 
+                 current_value: float = None):
+        self.config = config
+        if typology not in self.config['investment']['typology']:
+          types = self.config['investment']['typology']
+          raise ValueError(f'Invalid typology. Must be one of {list(types.keys())}')
 
         self.name = name
         self.typology = typology
+        self.typology_details = self.config['investment']['typology'][self.typology]
         self.initial_cost = initial_cost
         self.monthly_contribution = monthly_contribution
         self.current_value = current_value or initial_cost
@@ -30,8 +27,7 @@ class Investment:
         """
         Return a Gaussian distributed annual gain percentage based on the typology.
         """
-        typology_details = self.TOPOLOGY_DETAILS[self.typology]
-        return random.gauss(typology_details['mean'], typology_details['std_dev'])
+        return random.gauss(self.typology_details['mean'], self.typology_details['std_dev'])
 
 
     def step(self):
@@ -51,7 +47,7 @@ class Investment:
         self.historical_values.append(self.current_value)
         # dividend
         if self.typology=='stock' and self.months_since_beginning % 3 ==0:
-          return self.current_value*self.DIVIDEND/12*3 # annual->month  ####TODO: taxation
+          return self.current_value*self.config['investment']['dividend_yield']/12*3 # annual->month  ####TODO: taxation
 
     def add_contribution(self, amount):
         """
